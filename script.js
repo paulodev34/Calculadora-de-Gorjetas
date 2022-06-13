305bb602e4719c284f7ae81008a6c7878116fd5c
Paulo Sérgio <102154585+paulodev34@users.noreply.github.com>
2022-06-13 16:39:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 function calculateTip(event) {	
 	event.preventDefault();
 	let cont = document.getElementById('cont').value;	
-	let servicepessoas = document.getElementById('servicepessoas').value;
-	let numOfquality = document.getElementById('quality').value;
-	if(cont == '' | servicepessoas == 0){
+	let quality = document.getElementById('quality').value;
+	let mumOfpessoas = document.getElementById('pessoas').value;
+	if(cont == '' | quality == 0){
 	alert("Por favor, preencha os valores")
 	return;
 	}         
 
-	if(numOfquality == "" | numOfquality <= 1) {
-		numOfquality = 1;
+	if(mumOfpessoas == "" | mumOfpessoas <= 1) {
+		mumOfpessoas = 1;
 	document.getElementById('totalpeople').style.display = "none"
 	} 
 	else {
 	document.getElementById('totalpeople').style.display = "block"
 	}
 
-	let total = (cont * servicepessoas) / numOfquality;
+	let total = (cont * quality) / mumOfpessoas;
 	total = total.toFixed(2);
 	document.getElementById('total').innerHTML = total;
 	document.getElementById('totaltip').style.display = "block";
 }
 
-document.getElementById('totaltip').style.display = "none";
+
 document.getElementById('totalpeople').style.display = "none";
 
 document.getElementById('tipsform').addEventListener('submit', calculateTip);
